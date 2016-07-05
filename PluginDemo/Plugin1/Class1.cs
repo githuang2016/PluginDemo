279826c318d3dc4a4cbd5bb7eb315c33d7dbc2c1
githuang2016 <huangwel@outlook.com>
2016-07-05 09:11:47 +0800

    Add files via upload

---
+++ PluginDemo/Plugin1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Reflection.Com.Demo;
+
+namespace Plugin1
+{
+    public class Class1 : IPlugin
+    {
+        public void Excute()
+        {
+            Console.WriteLine("插件1已经安装成功");
+        }
+    }
+}
---
+++ PluginDemo/Plugin2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Reflection.Com.Demo;
+
+namespace Plugin2
+{
+    public class Class2 : IPlugin 
+    {
+        public void Excute()
+        {
+            Console.WriteLine("插件2已经安装成功");
+        }
+    }
+}
